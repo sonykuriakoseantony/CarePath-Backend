c4276e94e4acafd810702ac2e3c0fc652783f28b
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2026-02-06 01:17:44 +0530

    mail sending issue - port changed

--- config/mailer.js
+++ config/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
